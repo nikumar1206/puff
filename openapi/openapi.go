4d7b33f5ca2a69f7555b5928fc1993ac7bf5472e
Nikhil <nikumar1206@gmail.com>
2024-04-27 21:11:10 -0400

    commiting the rest of openapi

--- openapi/openapi.go
+++ openapi/openapi.go
@@ -1,5 +1,32 @@
 package openapi
 
+import "fmt"
+
 type OpenAPI struct {
-	name string
+	title string // title of the application
+}
+
+var OPENAPI_UI string = `<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Elements in HTML</title>
+  
+    <script src="https://unpkg.com/@stoplight/elements/web-components.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/@stoplight/elements/styles.min.css">
+  </head>
+  <body style="height: 100vh;">
+
+    <elements-api
+      apiDescriptionUrl="%.yaml"
+      router="hash"
+      tryItCredentialsPolicy="same-origin"
+    />
+
+  </body>
+</html>`
+
+func GenerateOpenAPIUI(path, title string) string {
+	return fmt.Sprintf(OPENAPI_UI, path, title)
 }
