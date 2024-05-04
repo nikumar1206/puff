ac1d08646062df644c195e2e2dda3568882dfd0f
Ajitesh Kumar <ajinest6@gmail.com>
2024-05-04 15:33:32 -0400

    Needs Description

--- route/route.go
+++ route/route.go
@@ -6,6 +6,7 @@ type Route struct {
 	Protocol string
 	Pattern  string
 	Path     string
+	Description string
 	Handler  func(request.Request) interface{}
 	// should probably have responses (200 OK followed by json, 400 Invalid Paramaters, etc...)
 }
