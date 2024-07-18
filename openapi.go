2c39806f12592ef59a72f5ae28ac84e751ce8fa4
Ajitesh Kumar <ajinest6@gmail.com>
2024-07-18 14:42:16 -0400

    no need to check ok, default value will be empty

--- openapi.go
+++ openapi.go
@@ -242,11 +242,7 @@ func addRoute(router Router, route Route, tags *[]Tag, tagNames *[]string, paths
 		Responses:   map[string]Response{},
 		Description: description, // TODO: needs to be dynamic on route
 	}
-	pathItem := PathItem{}
-	storedPathItem, ok := (*paths)[route.fullPath]
-	if ok {
-		pathItem = storedPathItem
-	}
+	pathItem := (*paths)[route.fullPath]
 	switch route.Protocol {
 	// TODO: handle other protocols
 	case http.MethodGet:
