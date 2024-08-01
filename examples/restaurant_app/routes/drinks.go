73944293755e368919eb0adec3cdc0d33db078b8
Ajitesh Kumar <ajinest6@gmail.com>
2024-07-31 22:47:36 -0400

    merge with mysel

--- examples/restaurant_app/routes/drinks.go
+++ examples/restaurant_app/routes/drinks.go
@@ -4,13 +4,22 @@ import (
 	"github.com/nikumar1206/puff"
 )
 
+type EchoInput struct {
+	Body string
+}
+
 func DrinksRouter() *puff.Router {
 	r := puff.NewRouter(
 		"Drinks",
 		"/drinks",
 	)
+	echoInput := new(EchoInput)
 
-	r.IncludeRouter(WaterRouter())
-	r.IncludeRouter(SodaRouter())
+	r.Get("/echo", "echos the body", echoInput, func(c *puff.Context) {
+		c.SendResponse(puff.GenericResponse{
+			Content: echoInput.Body,
+		})
+	})
+
 	return r
 }
