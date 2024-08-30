484280eb9b426ae03d4410bbeb23487ffbc0c3b2
Ajitesh Kumar <ajinest6@gmail.com>
2024-08-30 10:53:22 -0400

    documentation on functions + log error on error reading descriptions

--- app.go
+++ app.go
@@ -32,10 +32,23 @@ func (a *PuffApp) IncludeRouter(r *Router) {
 	a.RootRouter.IncludeRouter(r)
 }
 
+// Use registers a middleware function to be used by the root router of the PuffApp.
+// The middleware will be appended to the list of middlewares in the root router.
+//
+// Parameters:
+// - m: Middleware function to be added.
 func (a *PuffApp) Use(m Middleware) {
 	a.RootRouter.Middlewares = append(a.RootRouter.Middlewares, &m)
 }
 
+// addOpenAPIRoutes adds routes to serve OpenAPI documentation for the PuffApp.
+// If a DocsURL is specified, the function sets up two routes:
+// 1. A route to provide the OpenAPI spec as JSON.
+// 2. A route to render the OpenAPI documentation in a user-friendly UI.
+//
+// This method will not add any routes if DocsURL is empty.
+//
+// Errors during spec generation are logged, and the method will exit early if any occur.
 func (a *PuffApp) addOpenAPIRoutes() {
 	if a.DocsURL == "" {
 		return
@@ -70,6 +83,13 @@ func (a *PuffApp) addOpenAPIRoutes() {
 	a.IncludeRouter(&docsRouter)
 }
 
+// attachMiddlewares recursively applies middlewares to all routes within a router.
+// This function traverses through the router's sub-routers and routes, applying the
+// middleware functions in the given order.
+//
+// Parameters:
+// - middleware_combo: A pointer to a slice of Middleware to be applied.
+// - router: The router whose middlewares and routes should be processed.
 func attachMiddlewares(middleware_combo *[]Middleware, router *Router) {
 	for _, m := range router.Middlewares {
 		nmc := append(*middleware_combo, *m)
@@ -85,6 +105,9 @@ func attachMiddlewares(middleware_combo *[]Middleware, router *Router) {
 	}
 }
 
+// patchAllRoutes applies middlewares to all routes and sub-routers in the root router
+// of the PuffApp. It also patches the routes of each router to ensure they have been
+// processed for middlewares.
 func (a *PuffApp) patchAllRoutes() {
 	a.RootRouter.patchRoutes()
 	for _, r := range a.RootRouter.Routers {
@@ -93,6 +116,15 @@ func (a *PuffApp) patchAllRoutes() {
 	attachMiddlewares(&[]Middleware{}, a.RootRouter)
 }
 
+// ListenAndServe starts the PuffApp server on the specified address.
+// Before starting, it patches all routes, adds OpenAPI documentation routes (if available),
+// and sets up logging.
+//
+// If TLS certificates are provided (TLSPublicCertFile and TLSPrivateKeyFile), the server
+// starts with TLS enabled; otherwise, it runs a standard HTTP server.
+//
+// Parameters:
+// - listenAddr: The address the server will listen on (e.g., ":8080").
 func (a *PuffApp) ListenAndServe(listenAddr string) {
 	slog.SetDefault(a.Logger)
 	a.patchAllRoutes()
@@ -110,25 +142,63 @@ func (a *PuffApp) ListenAndServe(listenAddr string) {
 	}
 }
 
+// Get registers an HTTP GET route in the PuffApp's root router.
+//
+// Parameters:
+// - path: The URL path of the route.
+// - fields: Optional fields associated with the route.
+// - handleFunc: The handler function that will be executed when the route is accessed.
 func (a *PuffApp) Get(path string, fields any, handleFunc func(*Context)) {
 	a.RootRouter.registerRoute(http.MethodGet, path, handleFunc, fields)
 }
 
+// Post registers an HTTP POST route in the PuffApp's root router.
+//
+// Parameters:
+// - path: The URL path of the route.
+// - fields: Optional fields associated with the route.
+// - handleFunc: The handler function that will be executed when the route is accessed.
 func (a *PuffApp) Post(path string, fields any, handleFunc func(*Context)) {
 	a.RootRouter.registerRoute(http.MethodPost, path, handleFunc, fields)
 }
 
+// Patch registers an HTTP PATCH route in the PuffApp's root router.
+//
+// Parameters:
+// - path: The URL path of the route.
+// - fields: Optional fields associated with the route.
+// - handleFunc: The handler function that will be executed when the route is accessed.
 func (a *PuffApp) Patch(path string, fields any, handleFunc func(*Context)) {
 	a.RootRouter.registerRoute(http.MethodPatch, path, handleFunc, fields)
 }
 
+// Put registers an HTTP PUT route in the PuffApp's root router.
+//
+// Parameters:
+// - path: The URL path of the route.
+// - fields: Optional fields associated with the route.
+// - handleFunc: The handler function that will be executed when the route is accessed.
 func (a *PuffApp) Put(path string, fields any, handleFunc func(*Context)) {
 	a.RootRouter.registerRoute(http.MethodPut, path, handleFunc, fields)
 }
 
+// Delete registers an HTTP DELETE route in the PuffApp's root router.
+//
+// Parameters:
+// - path: The URL path of the route.
+// - fields: Optional fields associated with the route.
+// - handleFunc: The handler function that will be executed when the route is accessed.
 func (a *PuffApp) Delete(path string, fields any, handleFunc func(*Context)) {
 	a.RootRouter.registerRoute(http.MethodDelete, path, handleFunc, fields)
 }
+
+// WebSocket registers a WebSocket route in the PuffApp's root router.
+// This route allows the server to handle WebSocket connections at the specified path.
+//
+// Parameters:
+// - path: The URL path of the WebSocket route.
+// - fields: Optional fields associated with the route.
+// - handleFunc: The handler function to handle WebSocket connections.
 func (a *PuffApp) WebSocket(path string, fields any, handleFunc func(*Context)) {
 	newRoute := Route{
 		WebSocket: true,
@@ -140,6 +210,8 @@ func (a *PuffApp) WebSocket(path string, fields any, handleFunc func(*Context)) 
 	a.RootRouter.Routes = append(a.RootRouter.Routes, &newRoute)
 }
 
+// AllRoutes returns all routes registered in the PuffApp, including those in sub-routers.
+// This function provides an aggregated view of all routes in the application.
 func (a *PuffApp) AllRoutes() []*Route {
 	return a.RootRouter.AllRoutes()
 }
--- description.go
+++ description.go
@@ -1,22 +1,29 @@
 package puff
 
 import (
+	"fmt"
+	"log/slog"
 	"os"
 	"strings"
 )
 
+// readDescription reads comments based on the file and line number of
+// the caller that called the GET, POST, etc. methods on the router. It
+// will read upwards of the method call.
 func readDescription(file string, lineNumber int, ok bool) string {
 	if !ok {
+		slog.Error("puff/readDescription cannot read description: ok is false.")
 		return ""
 	}
 	srcfile, err := os.ReadFile(file)
 	if err != nil {
+		slog.Error(fmt.Sprintf("puff/readDescription cannot read description: os.ReadFile failed with error: %s", err.Error()))
 		return ""
 	}
 	lines := strings.Split(string(srcfile), "\n")
 	comments := []string{}
 	// read file in reverse
-	for i := lineNumber - 2; i >= 0; i-- {
+	for i := lineNumber - 2; i >= 0; i-- { // guess and check got us to line number - 2
 		line := strings.TrimSpace(lines[i])
 		if line == "" { // empty line (ignore)
 			continue
